a4f11f41316d22e1b330ff3c5138320632df2a49
danishaft <danielejeh2019@gmail.com>
2025-12-08 20:52:11 +0100

    Refactor Gatsby Node APIs by replacing gatsby-node.js with gatsby-node.ts, implementing TypeScript for page creation and node handling, and adding utility functions for project management and URL parameter manipulation.

---
+++ src/utils/project.ts
@@ -0,0 +1,26 @@
+import { Project, ProjectID, Projects } from '../types/Project';
+import { GitHubStars } from '../types/GitHubStars';
+import { Link } from '../types/Link';
+import { routes } from '../constants/routes';
+
+export function getGitHubProjectID(project: Project): string | null {
+  if (
+    !project
+    || !project?.gitHubRepo
+    || !project?.gitHubRepo?.repo
+    || !project?.gitHubRepo?.owner
+  ) {
+    return null;
+  }
+  return `${project.gitHubRepo.owner}/${project.gitHubRepo.repo}`;
+}
+
+export function projectMapToArray(projects: Projects): Project[] {
+    return Object.keys(projects)
+      .map<Project>((projectID: ProjectID) => {
+        const project: Project = { ...projects[projectID] };
+        // Make sure that the project ID is the same as the project key in the projects map.
+        project.id = projectID;
+        return project;
+      });
+  }
